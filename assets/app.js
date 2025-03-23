10de38df4595069019e2c8ac94227aabc00023e4
TxKaotic <daniels.stephen2011@yahoo.com>
2025-03-23 13:02:45 -0400

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+function jupiterCam(){
+    window.location='https://video-monitoring.com/beachcams/jupiter/';
+}
+function ftPierceCam(){
+    window.location='https://visitstlucie.com/fort-pierce-inlet-webcam-and-jetty/';
+}
+
+
+
